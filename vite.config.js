efb4c0eaa13ad091d79a64729c82cdfba9b6b1f1
Mitchell Bennett <mitchellbennett1967@yahoo.com>
2026-05-13 17:23:37 -0600

    changing the root from /dist to /docs

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/react-final/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
 })
