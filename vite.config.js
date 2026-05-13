9ceaf325cecaf6c57694b9a5df8a550e1569202f
Mitchell Bennett <mitchellbennett1967@yahoo.com>
2026-05-13 17:30:53 -0600

    another attempt at fixing folder issue from /doc to /root

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: 'app',
   plugins: [react()],
   base: '/react-final/',
+  publicDir: '../public',
   build: {
-    outDir: 'docs',
-    emptyOutDir: true,
+    outDir: '../',
+    emptyOutDir: false,
   },
 })
